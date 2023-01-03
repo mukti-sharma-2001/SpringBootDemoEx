d052e65479282ede48924795369a1d58b2a03b14
muktisharma <mukti.sharma@nagarro.com>
2023-01-03 13:33:49 +0530

    some changes in test

--- src/test/java/com/example/SpringbootAppEx/SpringbootAppExApplicationTests.java
+++ src/test/java/com/example/SpringbootAppEx/SpringbootAppExApplicationTests.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
