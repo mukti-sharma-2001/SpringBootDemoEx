1fbf56b052eabac515771ab8376dbb47d485eaaf
muktisharma <mukti.sharma@nagarro.com>
2023-01-03 13:28:28 +0530

    some changes in initializer

--- src/main/java/com/example/SpringbootAppEx/SpringbootAppExApplication.java
+++ src/main/java/com/example/SpringbootAppEx/SpringbootAppExApplication.java
@@ -1,6 +1,5 @@
 package com.example.SpringbootAppEx;
 
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
