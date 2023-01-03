f92948d5a097dce8690835958502fbf66c6805c0
muktisharma <mukti.sharma@nagarro.com>
2023-01-03 13:22:02 +0530

    some changes in initializer

--- src/main/java/com/example/SpringbootAppEx/SpringbootAppExApplication.java
+++ src/main/java/com/example/SpringbootAppEx/SpringbootAppExApplication.java
@@ -1,23 +1,13 @@
 package com.example.SpringbootAppEx;
 
-import java.util.Arrays;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.SpringbootAppEx.entities.Employee;
-import com.example.SpringbootAppEx.repository.EmpRepository;
-
 @SpringBootApplication
 public class SpringbootAppExApplication {
-	
-	@Autowired
-	EmpRepository repo;
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAppExApplication.class, args);
+				SpringApplication.run(SpringbootAppExApplication.class, args);
 	}
 
 
